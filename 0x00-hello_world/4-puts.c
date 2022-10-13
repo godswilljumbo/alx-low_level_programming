06a78046c2a160e0581f17fcf9ead19d9af3f555
Godswill Jumbo <godswill.j@outlook.com>
2022-10-13 10:22:56 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char test[] ="Programming is like building multilingual puzzle"
+	char test[] = "Programming is like building a multilingual puzzle"\n;
 
-		puts(test);
-	return 0;
+	puts(test);
+	return (0);
 }
