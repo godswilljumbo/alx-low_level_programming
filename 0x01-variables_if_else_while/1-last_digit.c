62647bc05a605f9b70726d5a738d9187d82e6cb7
Godswill Jumbo <godswill.j@outlook.com>
2022-10-14 16:02:33 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,12 +16,13 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater that 5\n", n, n % 10);
-	} else if ((n % 10) == 0)
-	{
-		printf("Last digit of %d is %d is 0\n", n, n % 10);
 	} else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit is %d is %d and is less than 6 and not 0\n", n, n % 10);
+	} 
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
 }
