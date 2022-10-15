5adddd9cb82829895408fafa3610d4da0cdc07fe
Godswill Jumbo <godswill.j@outlook.com>
2022-10-14 18:24:31 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char text;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (text = 'a'; text <= 'z'; text++)
+		putchar(text);
+	putchar('\n');
+	return (0);
+}
