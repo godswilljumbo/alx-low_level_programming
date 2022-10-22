5d440fe5a53c286c1e912303d0e931f3768046f8
Godswill Jumbo <godswill.j@outlook.com>
2022-10-22 16:50:43 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: chatacter to check
+ * Return: 1 and print "+" if n is greater than zero. Return 0 and print "0" if n is zero. Return -1 and print "-" if n is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
