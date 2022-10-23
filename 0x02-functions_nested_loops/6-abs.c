ac1358b74018e306c92b938bee3c7efb78c03717
Godswill Jumbo <godswill.j@outlook.com>
2022-10-22 17:29:32 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @g: integer to be converted
+ * Return: absolute value
+ */
+int _abs(int g)
+{
+	if (g < 0)
+		g = g * -1;
+	return (g);
+}
