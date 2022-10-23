cfcb8a4837f846532f9139ed3c806bde1064f241
Godswill Jumbo <godswill.j@outlook.com>
2022-10-23 04:12:49 -0700

    Sum two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - print the sum of two integers
+ * @x: int x
+ * @y: int y
+ * Return: sum of xy
+ */
+int add(int x, int y)
+{
+	int a;
+
+	a = (x + y);
+	return (a);
+}
