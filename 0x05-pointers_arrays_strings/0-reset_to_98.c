0f390c063d1131f6ad136cfd9f564b86f3c4797d
Godswill Jumbo <godswill.j@outlook.com>
2022-10-24 12:48:26 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - update the value of an integer.
+ * @n: pointer to integer to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
