e0df38960f6e8e854a9f773eef2ff6bc6b4fe049
Godswill Jumbo <godswill.j@outlook.com>
2022-10-25 18:42:45 -0700

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ * @s:
+ */
+
+void print_rev(char *s)
+{
+	int g = 0;
+
+	while (*(s + g) != '\0')
+		g += 1;
+	g -= 1;
+	while (g >= 0)
+	{
+		_putchar(*(s + g));
+		g--;
+	}
+	_putchar('\n');
+}
