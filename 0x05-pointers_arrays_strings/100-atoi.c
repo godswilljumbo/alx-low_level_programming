8844853db762ff847e00e512033bc6fa0b7fdbc1
Godswill Jumbo <godswill.j@outlook.com>
2022-10-26 12:59:13 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _atoi - function
+ * @s: string
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int res = 0;
+
+	int sign = 1;
+
+	int i = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= 1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
+}
