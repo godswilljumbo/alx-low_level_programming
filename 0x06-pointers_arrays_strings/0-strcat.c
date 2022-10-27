c67688b991a2ada115dead0e4ea718cd5b7258e1
Godswill Jumbo <godswill.j@outlook.com>
2022-10-27 05:11:48 -0700

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - function
+ * @dest: a pointer
+ * @src: a pointer
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+	
+	for (x = 0; *dest != 0; x++)
+		dest++;
+	
+	for (y = 0; src[y] != 0; y++)
+	{
+		*dest = src[y];
+		dest++;
+	}
+	*dest = '\0';
+	dest -= (x + y);
+	return (dest);
+}
+	
