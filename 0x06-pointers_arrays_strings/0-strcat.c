bb7847360c9e37cd9e7dd8627ebd91feb8e1b461
Godswill Jumbo <godswill.j@outlook.com>
2022-10-27 05:42:45 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * *_strcat - function
  * @dest: a pointer
  * @src: a pointer
  * Return: Always 0
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int x, y;
-	
+
 	for (x = 0; *dest != 0; x++)
 		dest++;
-	
 	for (y = 0; src[y] != 0; y++)
 	{
 		*dest = src[y];
@@ -24,4 +21,3 @@ char *_strcat(char *dest, char *src)
 	dest -= (x + y);
 	return (dest);
 }
-	
