1238d1f0a798201eacd1665b7dc00aa653a98880
Godswill Jumbo <godswill.j@outlook.com>
2022-10-26 17:09:20 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,62 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void randomPasswordGeneration(int N)
+{
+	int g = 0;
+
+	int randomizer = 0;
+
+	srand((unsigned int) (time(NULL)));
+
+	char numbers[] = "0123456789";
+
+	char letter[] = "abcdefghijklmnopqrstuvwxyz";
+
+	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	char symbols[] = "?!&%*@$^€¥_";
+
+	char password[N];
+
+	randomizer = rand () % 4;
+
+	for (g = 0; g < N; g++)
+	{
+		if (randomizer == 1)
+		{
+			password[g] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[g]);
+		}
+		else if (randomizer == 2)
+		{
+			password[g] = LETTER[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[g]);
+		}
+		else if (randomizer == 3)
+		{
+			password[g] = symbols[rand() % 11];
+			randomizer = rand() % 4;
+			printf("%c", password[g]);
+		}
+		else
+		{
+			password[g] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[g]);
+		}
+	}
+}
+
+int main()
+{
+	int N = 10;
+
+	randomPasswordGeneration(N);
+
+	return (0);
+}
