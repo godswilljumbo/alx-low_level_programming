5674a358e494c44adc77ed7821de92cdc200f16d
Godswill Jumbo <godswill.j@outlook.com>
2022-11-15 09:16:32 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - struct dog typedef
+ */
+typedef struct dog dog_t;
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
