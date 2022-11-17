bb06b88e551637159a7050403655ceae187f878f
Godswill Jumbo <godswill.j@outlook.com>
2022-11-17 14:47:39 -0800

    Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all _ function
+ * @n: number
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int b;
+	va_list val;
+
+	if (n == 0)
+		return (0);
+	va_start(val, n);
+	for (b = 0; b < n; b++)
+		sum += va_arg(val, int);
+	va_end(val);
+	return (sum);
+}
