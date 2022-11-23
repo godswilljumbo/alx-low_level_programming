674acf356eb69fe612e28c6ea6e5f8e05b22d2f6
Godswill Jumbo <godswill.j@outlook.com>
2022-11-23 09:12:49 -0800

    Header

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
