7cea1fcfbfbc6b7828cb67438b74fd1254932911
Godswill Jumbo <godswill.j@outlook.com>
2022-11-24 20:26:53 -0800

    Add node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function
+ * @head: pointer
+ * @str: string
+ * Return: address of new elememt or NULL if fail
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *don = *head;
+	list_t *add;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	add = malloc(sizeof(list_t));
+	if(!add)
+		return (NULL);
+	add->str = strdup(str);
+	add->len = len;
+	add->next = NULL;
+	if (*head == NULL)
+	{
+		*head = add;
+		return (add);
+	}
+	while (don->next)
+		don = don->next;
+	don->next = add;
+	return (add);
+}
+
