cd7d12d3ddf4a9a7314e9a94a96f7a118cbb24fd
Godswill Jumbo <godswill.j@outlook.com>
2022-11-25 08:29:06 -0800

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 	new_node = malloc(sizeof(list_t));
-	if(!new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = len;
