3f9bd4a77ed114bb8f9e6aa58f7d0a569fdeaf6f
Godswill Jumbo <godswill.j@outlook.com>
2022-11-24 21:36:20 -0800

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,10 @@
 void print_before_main(void) __attribute__ ((constructor));
 
 /**
- * print_before_msin - print things before main
+ * print_before_main - print things before main
  */
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
