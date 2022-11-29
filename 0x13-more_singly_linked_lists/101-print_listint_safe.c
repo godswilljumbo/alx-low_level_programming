21dfdd694fd3b21d62e465c4f5d41bdc007057bc
Godswill Jumbo <godswill.j@outlook.com>
2022-11-29 15:18:26 -0800

    Print(safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t g;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (g = 0; g < size - 1; g++)
+		newlist[g] = list[g];
+	newlist[g] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t g, nm = 0;
+	const listint_t **list = NULL;
+
+	while (head != NULL)
+	{
+		for (g = 0; g < nm; g++)
+		{
+			if (head == list[g])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (nm);
+			}
+		}
+		nm++;
+		list = _r(list, nm, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	free(list);
+	return (nm);
+}
