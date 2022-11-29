9a8e36bdd1baf39fe0b76172302e7669ba553551
Godswill Jumbo <godswill.j@outlook.com>
2022-11-29 01:13:49 -0800

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the linked list
+ *
+ * Return: number of elements in a linked listint_t list
+ */
+ size_t listint_len(const listint_t *h)
+{
+	size_t all = 0;
+
+	while (h != NULL)
+	{
+		all++;
+		h = h->next;
+	}
+	return (all);
+}
