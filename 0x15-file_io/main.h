b699891be78fd642d6b3f803c5d4993253a23254
Godswill Jumbo <godswill.j@outlook.com>
2022-12-05 14:39:41 -0800

    Header Updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h> 
+#include <unistd.h>
 
 /* My prototype functions are below */
 
