e49383eb31b7583414ea4f8089ba8e28610c4195
Godswill Jumbo <godswill.j@outlook.com>
2022-12-07 17:08:33 -0800

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int bob = 0;
+	int fd = 0;
+	int result = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[bob] != '\0')
+			bob++;
+	}
+	else
+	{
+		close(fd);
+		return (1);
+	}
+
+	result = write(fd, text_content, bob);
+	if (result == -1 || result != bob)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
